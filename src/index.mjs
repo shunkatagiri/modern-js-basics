0ed6f34e07f26588b12b49e574e24dc99fad94b8
shunkatagiri <108392107+shunkatagiri@users.noreply.github.com>
2023-08-11 16:13:51 +0900

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,47 @@
+/**
+ * const,let等の変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1)
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能性
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 ="let変数"　;
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 =  "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// //const変数は上書き不可
+// //  val3 = "const変数を上書き"
+
+// const val3 = "const変数を再宣言";
+
+
+// const val4 = {
+//   name: "ジャケえ",
+//   age: 28,
+// };
+// val4.name = "jek";
+// val4.adress = "Hiroshima";
+// console.log(val4);
+
+// const val5 = ['dog', 'cat'];
+// val5[0] = 'bird'
+// val5.push("mokey");
+// console.log(val5);
+
+
